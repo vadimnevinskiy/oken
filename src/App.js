151f71dbb6a844f8073307f2c374afe2aa244ca5
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-06-28 14:13:37 +0500

    Libraries installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import 'materialize-css'
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="container">
+        <h1>Library</h1>
     </div>
   );
 }
